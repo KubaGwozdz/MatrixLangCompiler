7dda2cb517362cff3022885b49ae1b68ef0b4626
KubaGwozdz <jakub.gwozdz@icloud.com>
2019-05-13 18:59:59 +0200

    interpreter initial commit

--- Interpreter.py
+++ Interpreter.py
@@ -7,8 +7,11 @@ import sys
 
 sys.setrecursionlimit(10000)
 
+
 class Interpreter(object):
 
+    def __init__(self):
+        self.memoryStack = MemoryStack()
 
     @on('node')
     def visit(self, node):
@@ -18,10 +21,7 @@ class Interpreter(object):
     def visit(self, node):
         r1 = node.left.accept(self)
         r2 = node.right.accept(self)
-        # try sth smarter than:
-        # if(node.op=='+') return r1+r2
-        # elsif(node.op=='-') ...
-        # but do not use python eval
+        return eval("a" + node.op + "b", {"a": r1, "b": r2})
 
     @when(AST.AssInstr)
     def visit(self, node):
